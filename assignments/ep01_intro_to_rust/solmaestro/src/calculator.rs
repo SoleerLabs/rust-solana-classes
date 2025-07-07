1c77fc1bb8dd0c3575f67da28cde225fd5b3c396
SolMaestro <simplymufasah@gmail.com>
2025-07-07 13:41:08 +0100

    refactored code to seperate modules to simplify main

---
+++ assignments/ep01_intro_to_rust/solmaestro/src/calculator.rs
@@ -0,0 +1,56 @@
+use std::io;
+use rand::Rng;
+
+pub fn subtraction () {
+
+    println!("Please enter a number for subtraction: ");
+
+     let random_number = rand::thread_rng().gen_range(1000, 5001);
+
+    let mut user_number = String::new();
+    io::stdin().read_line(&mut user_number).expect("Failed to readline");
+
+    let number: i64 = user_number.trim().parse().expect("Invalid number");
+   
+    println!("{} - {} is = {}", random_number, number, random_number - number);
+
+}
+
+pub fn addition () {
+    println!("Please enter a number for addition: ");
+
+     let random_number = rand::thread_rng().gen_range(1000, 5001);
+
+    let mut user_number = String::new();
+    io::stdin().read_line(&mut user_number).expect("Failed to readline");
+
+    let number: i64 = user_number.trim().parse().expect("Invalid number");
+
+    println!("{} + {} is = {}", random_number, number, random_number + number);
+}
+
+pub fn multiplication () {
+    println!("Please enter a number for multiplication: ");
+
+     let random_number = rand::thread_rng().gen_range(1000, 5001);
+
+    let mut user_number = String::new();
+    io::stdin().read_line(&mut user_number).expect("Failed to readline");
+
+    let number: i64 = user_number.trim().parse().expect("Invalid number");
+
+    println!("{} * {} is = {}", random_number, number, random_number * number);
+}
+
+pub fn division () {
+     println!("Please enter a number for division: ");
+
+     let random_number = rand::thread_rng().gen_range(1000, 5001);
+
+    let mut user_number = String::new();
+    io::stdin().read_line(&mut user_number).expect("Failed to readline");
+
+    let number: i64 = user_number.trim().parse().expect("Invalid number");
+
+    println!("{} / {} is = {}", random_number, number, random_number / number);
+}
